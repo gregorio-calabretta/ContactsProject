2676587844a4a8445a3bc5c86cb1285221c83ac8
gregoriocalabretta- <gregoriocalabretta94@gmail.com>
2024-02-08 02:59:00 +0100

    Get Mapping return null

--- src/main/java/com/example/ContactsProject/controller/Controller.java
+++ src/main/java/com/example/ContactsProject/controller/Controller.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Controller {
     @GetMapping("/Contacts")
     public List<Contact> showContacts(){
-        ResponseEntity<>
+        return null;
     }
 
 }
